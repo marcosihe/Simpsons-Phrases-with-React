d6e584eca4536aeedb366cbb741efe6c3032b823
marcosihe <marcosherrera.ib@gmail.com>
2021-06-20 14:08:45 -0300

    Se agrego el componente spinner y la logica necesaria para su funcionamiento, incluyendo un setTimeout para mejorar la experiencia del usuario.

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
 import { useState, useEffect } from "react";
 
 function App() {
 const [personaje, setPersonaje] = useState({});
+const [spinnerValue, setSpinnerValue] = useState(false);
 
 useEffect( () => {
   consultarApi()
 },[]);
 
 const consultarApi = async() => {
+  setSpinnerValue(true);
   const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
   const resultado = await respuesta.json();
   setPersonaje(resultado[0]);
+  setTimeout( () => setSpinnerValue(false), 2000);
 }
+
+const mostrarComponente = (spinnerValue === true) ? <Spinner></Spinner> : <Frase personaje={personaje}></Frase>;
 
   return (
     <section className="container my-5 text-center">
@@ -27,7 +33,7 @@ const consultarApi = async() => {
           />
         </figure>
         <Button variant="warning" onClick={ () => consultarApi()}>Obtener frase</Button>
-        <Frase personaje={personaje}></Frase>
+        {mostrarComponente}
     </section>
   );
 }
